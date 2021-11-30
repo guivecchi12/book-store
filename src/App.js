e9ce5a26f570b8d47095ece4f993fd680e7a2886
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-11-29 19:36:27 -0700

    Frontend mostly done

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import data from './data';
+
+// Components
+import Navigation from './components/Navigation';
+import Products from './components/Products';
+import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from './contexts/CartContext';
+
+function App() {
+	const [products] = useState(data);
+	const [cart, setCart] = useState([]);
+
+	const addItem = item => {
+		// add the given item to the cart
+	
+		setCart(oldCart => [...oldCart, item]);
+	};
+	// console.log(cart);
+	// console.log ({ProductContext});
+
+	return (
+		<div className="App">
+			<ProductContext.Provider value = {{ products, addItem }}>
+				<CartContext.Provider value = { cart }>
+					<Navigation />
+
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
+		</div>
+	);
+}
+
+export default App;
